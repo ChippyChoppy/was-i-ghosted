87a0b3ce2ceb771057479691bb29bf596382c406
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-04-13 22:10:52 -0400

    styled components

--- src/Containers/Landing.js
+++ src/Containers/Landing.js
@@ -1,16 +1,41 @@
+import { getElementError } from '@testing-library/dom'
 import React from 'react'
 import landingvideo from '../Videos/landingvideo.mp4'
+import styled from 'styled-components'
 
 
 const Landing = () => {
     return (
         <div>
-            <video autoplay muted loop  >
+            <LandingVideo autoplay muted loop >
                 <source src={landingvideo} type="video/mp4" />
-            </video>
-            <button> Come on in! Let's find out!</button>
+            </LandingVideo>
+            <EnterButton > Come on in! Let's find out!</EnterButton>
         </div>
     )
 }
 
 export default Landing
+
+const LandingVideo = styled.video `
+  object-fit: cover;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  `
+// const LandingDiv = styled.div `
+//     height: 100%
+//     display: flex;
+//     flex-flow: row-reverse column-reverse;
+// `
+
+
+const EnterButton = styled.button`
+  position: fixed;
+  height: 35px;
+  text-align: center;
+  display: flex;
+  align-self: flex-end;  
+`
