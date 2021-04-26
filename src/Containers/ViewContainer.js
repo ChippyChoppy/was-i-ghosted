8c6a13799d02ba03fca81e9415f8173c0bb309b0
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-04-26 17:07:21 -0400

    router started

--- src/Containers/ViewContainer.js
+++ src/Containers/ViewContainer.js
@@ -1,22 +1,35 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 import Landing from './Landing'
 import QuestionsContainer from './QuestionsContainer'
 import Results from './Results'
+import { Switch, Route } from 'react-router-dom'
 
 const ViewContainer = () => {
-    const [ghosted, setGhosted] = useState(true)
+    const [ghosted, setGhosted] = useState(false)
     console.log(ghosted)
 
     let ghostedArray = ["sucks", "to", "be", "you"]
     let notGhostedArray = ["you", "don't", "suck", "they", "do"]
 
     return (
-        <div>
-            {/* <Landing /> */}
-            <QuestionsContainer isGhosted={setGhosted} />
-            
-            {ghosted ? <Results ghosted={ghostedArray} /> : null}
-        </div>
+        <>
+            <Switch>
+                <Route path='/welcome'>
+                    <Landing />
+                </Route>
+                <Route path='/questions'>
+                    <QuestionsContainer isGhosted={setGhosted} />
+                </Route>
+                <Route path='/results'>
+                    {ghosted ? <Results ghosted={ghostedArray} /> : <Results ghosted={notGhostedArray} />}
+                </Route>
+                
+
+                
+            </Switch>
+
+        </>
+
     )
 }
 
