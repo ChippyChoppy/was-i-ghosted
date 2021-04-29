2ea2ead0afe719e52376a32efcd7e86a45e5e9a3
Brian Sahota <bpsahota@gmail.com>
2021-04-29 14:16:51 -0400

    mvp reached

--- src/Containers/Results.js
+++ src/Containers/Results.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react'
 
 const Results = ({resultsArr}) => {
-console.log(resultsArr )
-
-    const ghostedResults = () => {
-        // const randomQuotes = Math.floor(Math.random() * resultsArr.length - 1)
-        // const quoteDisplay = 
-        return resultsArr[Math.floor(Math.random() * resultsArr.length)]
+    const [ affirmation, setAffirmation ] = useState(resultsArr[Math.floor(Math.random() * resultsArr.length)])
+        console.log(resultsArr )
+        
+        const ghostedResults = () => {
+            setAffirmation(resultsArr[Math.floor(Math.random() * resultsArr.length)])
     }
 
     return (
         <div>
             <h1>Not ghosted</h1>
-            <p>{ghostedResults()}</p>
+            <p>{affirmation}</p>
             <button onClick={ghostedResults}>Validate me again plz</button>
         </div>
     )
