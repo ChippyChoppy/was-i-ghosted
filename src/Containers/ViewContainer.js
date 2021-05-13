42eca7a560b44ff16e5c47099a93f99cd62d5590
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-05-12 20:12:15 -0400

    fixed router

--- src/Containers/ViewContainer.js
+++ src/Containers/ViewContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Landing from './Landing'
 import QuestionsContainer from './QuestionsContainer'
 import Results from './Results'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { ghostedArray } from './GhostedArray'
 import {notGhostedArray } from './NotGhostedArray'
 
@@ -13,7 +13,15 @@ const ViewContainer = () => {
     return (
         <>
             <Switch>
-                <Route path='/'>
+            <Route 
+                exact path="/"
+                render={() => {
+                    return (
+                        <Redirect to="/home" />
+                    )
+                }}
+            />      
+                <Route path='/home'>
                     <Landing />
                 </Route>
                 <Route path='/questions'>
