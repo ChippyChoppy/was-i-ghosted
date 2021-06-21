3b75247de6b9e878ba89195e6273875153255e97
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-06-20 22:59:01 -0400

    added to not ghosted array of affirmations

--- src/Containers/NotGhostedArray.js
+++ src/Containers/NotGhostedArray.js
@@ -5,5 +5,10 @@ export const notGhostedArray = [
     "Sure, there are plenty of fish in the sea, but you're not a fish. You're a human being, so I don't know why we're talking about fish... ",
     "Dating is hard, and it's easy to feel ghosted when things don't work out, but you weren't ghosted! So get back out there with the confidence of 100 hotties!",
     "Get back out there with your bad self!",
-    "We're still in the midst of a pandemic, none of us remember how to date anymore!"
+    "We're still in the midst of a pandemic, none of us remember how to date anymore!",
+    "Sometimes it's just a mutual decision to never speak again, that's where you are, even if you haven't realized it yet.",
+    "Every rejection is just leading you closer to acceptance.",
+    "You're a bad b*tch, get back out there!",
+    "Dating is hard, but you weren't ghosted, so keep on going!",
+    "They didn't text you, true, but did you text them?"
 ]
